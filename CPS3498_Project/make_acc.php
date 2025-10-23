4c35911f41975cb52f4a024bf45f9ed9ca5e3f5e
Jessebalves <152413074+Jessebalves@users.noreply.github.com>
2025-10-23 18:50:39 -0400

    Add files via upload

--- CPS3498_Project/make_acc.php
+++ CPS3498_Project/make_acc.php
@@ -2,12 +2,16 @@
 include "database_config.php";
 $connection = mysqli_connect($host,$username,$password,$dbname) or die;
 
+
 $user = $_POST["login_username"];
 $pass = $_POST["login_password"];
+$hashedPassword = password_hash($pass, PASSWORD_DEFAULT);
 
-$sql_query = "Insert into 2025F_CPS3498_01.login_info(username,password) values('$user','$pass')"; 
+$sql_query = "Insert into 2025F_CPS3498_01.login_info(username,password) values('$user','$hashedPassword')"; 
 
 $result = mysqli_query($connection, $sql_query); 
+
+
 try{
 if($result){
 	echo "Successfully created an account!";
--- CPS3498_Project/testo.php
+++ CPS3498_Project/testo.php
@@ -12,21 +12,32 @@
 	$connection = mysqli_connect($host,$username,$password,$dbname) or die;
 
 	$user = $_POST["login_username"];
-	$pass = $_POST["login_password"];
+	$pass = $_POST["login_password"]; 
+	
 
-	$sql_query = "Select username, password from 2025F_CPS3498_01.login_info where username = '$user' and password in ('$pass')"; 
+	$sql_query = "Select password from 2025F_CPS3498_01.login_info where username = '$user'"; 
 
 	$result = mysqli_query($connection, $sql_query); 
 
 	if($result){
-		if(mysqli_num_rows($result) == 1){
-			echo "<h1 style = 'text-align:center;'><u>Welcome to Team 2's Webpage!</u></h1>";
-			echo "<div class = 'container'>";
-			echo "<h3 style = 'text-align:center;'>Encryption</h3>";
-			echo "<img src = 'Encry.png' width ='90%' style = 'display:flex;justify-content:center;align-items:center;'>"; 
-			echo "<p> Encryption is the action of turning Plaintext into Ciphertext, and there are many different methods of achieving this.</p>";
-			echo "</div>";
-		
+		if(mysqli_num_rows($result) > 0){
+			while($rows = mysqli_fetch_array($result)){
+				$stored = $rows['password'];
+				if (password_verify($pass, $stored)){
+					echo "Password is valid.";
+					echo "Welcome back $user.";
+					echo "<h1 style = 'text-align:center;'><u>Welcome to Team 2's Webpage!</u></h1>";
+					echo "<div class = 'container'>";
+					echo "<h3 style = 'text-align:center;'>Encryption</h3>";
+					echo "<img src = 'Encry.png' width ='90%' style = 'display:flex;justify-content:center;align-items:center;'>"; 
+					echo "<p> Encryption is the action of turning Plaintext into Ciphertext, and there are many different methods of achieving this.</p>";
+					echo "</div>";
+					echo "logout";
+
+    				} else {
+        				echo "<h2 style = 'text-align:center;'>You are not authorized to view this page's contents.<h2>";
+    				}
+			}
 		}
 		else{
 			echo "<h2 style = 'text-align:center;'>You are not authorized to view this page's contents.<h2>"; 
