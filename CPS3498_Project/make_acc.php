3cd05805b7598a67e3e7911033c425fab9bc6706
Jessebalves <152413074+Jessebalves@users.noreply.github.com>
2025-10-23 15:22:07 -0400

    Add files via upload

---
+++ CPS3498_Project/make_acc.php
@@ -0,0 +1,28 @@
+<?php
+include "database_config.php";
+$connection = mysqli_connect($host,$username,$password,$dbname) or die;
+
+$user = $_POST["login_username"];
+$pass = $_POST["login_password"];
+
+$sql_query = "Insert into 2025F_CPS3498_01.login_info(username,password) values('$user','$pass')"; 
+
+$result = mysqli_query($connection, $sql_query); 
+try{
+if($result){
+	echo "Successfully created an account!";
+	echo "<a href='login_page.html'><p>Back to login page</p></a>";
+}
+else{
+	echo "Failure occurred";
+}
+}
+
+catch(Exception $e){
+	echo "Exception homie";
+}
+
+
+mysqli_close($connection); 
+
+?>
