269e885826ca4f9186a1ad07e19c2aee050d3789
Jessebalves <152413074+Jessebalves@users.noreply.github.com>
2025-10-31 23:27:28 -0400

    Add files via upload

--- CPS3498_Project/testo.php
+++ CPS3498_Project/testo.php
@@ -11,8 +11,8 @@
 
 	$connection = mysqli_connect($host,$username,$password,$dbname) or die;
 
-	$user = $_POST["login_username"];
-	$pass = $_POST["login_password"]; 
+	$user = mysqli_real_escape_string($connection,$_POST["login_username"]);
+	$pass = mysqli_real_escape_string($connection,$_POST["login_password"]); 
 	
 
 	$sql_query = "Select password from 2025F_CPS3498_01.login_info where username = '$user'"; 
@@ -24,15 +24,14 @@
 			while($rows = mysqli_fetch_array($result)){
 				$stored = $rows['password'];
 				if (password_verify($pass, $stored)){
-					echo "Password is valid.";
-					echo "Welcome back $user.";
+					echo "Welcome back $user.<br>";
+					echo "<a href='logout.php'>logout</a>";
 					echo "<h1 style = 'text-align:center;'><u>Welcome to Team 2's Webpage!</u></h1>";
 					echo "<div class = 'container'>";
 					echo "<h3 style = 'text-align:center;'>Encryption</h3>";
 					echo "<img src = 'Encry.png' width ='90%' style = 'display:flex;justify-content:center;align-items:center;'>"; 
 					echo "<p> Encryption is the action of turning Plaintext into Ciphertext, and there are many different methods of achieving this.</p>";
 					echo "</div>";
-					echo "logout";
 
     				} else {
         				echo "<h2 style = 'text-align:center;'>You are not authorized to view this page's contents.<h2>";
