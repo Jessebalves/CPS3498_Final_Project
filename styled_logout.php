3bbbbec06fd128474b85d17547aa36959724c296
elereico28 <paolereico@gmail.com>
2025-11-05 09:45:05 -0500

    Add files via upload

---
+++ styled_logout.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<h3 style= 'text-align:center;  font-size: 1em; color: #003366;'>You have successfully logged out.</h3>";
+
+//Do something with cookies here!
+echo "<a href='login_page.html'><p style = 'text-align:center;  font-size: 1em; color: #003366;'>Click here to go back to login</p></a>";
+?>
+
+<!DOCTYPE html>
+<title>Logout</title>
+  <meta charset="UTF-8">
+  <style>
+    body {
+      background-color: #cce6ff;
+      font-family: 'Segoe UI', sans-serif;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+
+    }
+  </style>
